8abda80efad1d0cf44e1eef9b5cb596c931459e3
m3lc <melas.christos@gmail.com>
2015-11-08 15:04:25 +0200

    feature (em-app1) 14. Replace the model with a deferred promise, that takes a while. 15. Modify template to show caption "loading....", a spinner could have been used. Needs to populate model in setupController and modify template. - model of routes uses promises, loading event fired on the closest route,   if don't want to block can move logic elsewhere in the flow i.e.   setupController

--- em-app1/js/app.js
+++ em-app1/js/app.js
@@ -6,12 +6,41 @@ App.Router.map(function(){
 });
 
 App.ApplicationRoute = Em.Route.extend({
-	model:function(){
-		return [
-		{id:1,name:"Home",url:"#/"},
-		{id:1,name:"Images",url:"#/images"},
-		{id:1,name:"About",url:"#/about"}
-		];
+	// model:function(){
+	// 	var defer = new $.Deferred();
+
+	// 	setTimeout(function(){
+	// 		var data =  [
+	// 				{id:1,name:"Home",url:"#/"},
+	// 				{id:1,name:"Images",url:"#/images"},
+	// 				{id:1,name:"About",url:"#/about"}
+	// 			];
+	// 			defer.resolve(data);
+	// 	},3000);
+
+	// 	return defer.promise();
+	// 	// return [
+	// 	// {id:1,name:"Home",url:"#/"},
+	// 	// {id:1,name:"Images",url:"#/images"},
+	// 	// {id:1,name:"About",url:"#/about"}
+	// 	// ];
+	// },
+	setupController:function(controller,model){
+		var defer = new $.Deferred();
+
+		setTimeout(function(){
+			var data =  [
+					{id:1,name:"Home",url:"#/"},
+					{id:1,name:"Images",url:"#/images"},
+					{id:1,name:"About",url:"#/about"}
+				];
+				controller.set("model",data);
+		},3000);
+	},
+	actions:{
+		loading:function(){
+			console.log("loading....");
+		}
 	}
 // 	// renderTemplate:function(){
 // 	// 	this._super.apply(this,arguments);
