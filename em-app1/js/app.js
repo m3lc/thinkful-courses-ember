a50df9ed68d2b180bbb76133e4b45650f41b0dfc
m3lc <melas.christos@gmail.com>
2015-11-08 14:53:34 +0200

    feature (em-app1) 7. Plan to create a sidebar as a menu and main content for the application. 8. Create a static menu inside the application template (.list-group , .list-group-item). 9. Add routes in App.Router, mention the deprecation of resources (use this.route('top-level-route', {resetNamespace: true});). 10. Replace the static menu with a template and use named outlets by overriding the ApplicationRoute. 11. Comment out the ApplicationRoute and use the render hbs helper. 12. Comment out the render helper and user the partial hbs helper. 13. Partial is reusable but component can be backed up with its generic component class. Comment out the partial helper and use a component. Create tmpl and use it. 14. Make the menu dynamic by creating model function in ApplicationRoute. 15. Modify component to accept the model as parameter.
    - common hbs helpers and component with implicit creation of class, model
      in routers to supply the template with data

--- em-app1/js/app.js
+++ em-app1/js/app.js
@@ -1,1 +1,20 @@
 var App = Em.Application.create();
+
+App.Router.map(function(){
+	this.route("about");
+	this.route("images");
+});
+
+App.ApplicationRoute = Em.Route.extend({
+	model:function(){
+		return [
+		{id:1,name:"Home",url:"#/"},
+		{id:1,name:"Images",url:"#/images"},
+		{id:1,name:"About",url:"#/about"}
+		];
+	}
+// 	// renderTemplate:function(){
+// 	// 	this._super.apply(this,arguments);
+// 	// 	this.render("menu",{into:"application",outlet:"menu"});
+// 	// }
+});
