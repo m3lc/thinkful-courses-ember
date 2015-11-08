b0a3cc327a30b5ad1240f4a700514640744924f7
m3lc <melas.christos@gmail.com>
2015-11-08 17:47:14 +0200

    feature (em-app1) 18. Get images from lorempixel by using deferred with delay in order to show the loading template. 19. Show the implicit creation of all routes if empty function is added in route. 20. Plan to show image by creating the route first and make the url work and then add events. 21. ΞυΞιIf your user interface is nested, your routes should be nestedΞυΞι ΞυΞι Yehuda Katz 22. Add image route, template and outlet in parent route. Also added calculated property to format date created. Use link-to with and without id, to show passing through the mode always and not passing respectively.
    - first created the url->route then events, showed implicit creation of
      sub routes
    
      23. Use didInsertElement event to modify the DOM of Image in Image
      component.
      24. Use afterRender to modify the DOM of Image in Images component. ****
      move this to session2!!
      i.e.
      Ember.View.reopen({
        didInsertElement : function(){
            this._super();
    	    Ember.run.scheduleOnce('afterRender', this,
    	    this.afterRenderEvent);
    	      },
    	        afterRenderEvent : function(){
    		    // implement this hook in your own subclasses and run
    		    // your jQuery logic there
    		      }
    		      });
    
    		      25. Add query parameters related to filtering. Add a
    		      field and filter based on description.
    		      26. Modify the routes to change how the image is
    		      presented. Change template and controller with
    		      index.
    		      27. Show classNames, classNameBindings and
    		      attributeBindings.
    
    		      - showed proper approach of modifying the DOM,
    			showed query parameters and changed presentation
    			with the route/subroute - templates relationship

--- em-app1/js/app.js
+++ em-app1/js/app.js
@@ -2,7 +2,9 @@ var App = Em.Application.create();
 
 App.Router.map(function(){
 	this.route("about");
-	this.route("images");
+	this.route("images",function(){
+		this.route("image",{path:":id"});
+	});
 });
 
 App.ApplicationRoute = Em.Route.extend({
@@ -26,7 +28,7 @@ App.ApplicationRoute = Em.Route.extend({
 	// 	// ];
 	// },
 	setupController:function(controller,model){
-		var defer = new $.Deferred();
+		var defer = new Em.$.Deferred();
 
 		setTimeout(function(){
 			var data =  [
@@ -35,15 +37,97 @@ App.ApplicationRoute = Em.Route.extend({
 					{id:1,name:"About",url:"#/about"}
 				];
 				controller.set("model",data);
-		},3000);
+		},0);
 	},
 	actions:{
 		loading:function(){
 			console.log("loading....");
+			return true;
 		}
 	}
 // 	// renderTemplate:function(){
 // 	// 	this._super.apply(this,arguments);
 // 	// 	this.render("menu",{into:"application",outlet:"menu"});
 // 	// }
+});
+
+App.ImagesRoute = Em.Route.extend({
+	model:function(){
+	var defer = new Em.$.Deferred();
+
+		setTimeout(function(){
+			var data =  [];
+
+			var ImageRecord = Em.Object.extend({
+				dateCreated:null,
+				dateCreatedCalc:function(){
+					var d = new Date(this.get("dateCreated"));
+					var dFmt = (d.getMonth()+1)+"/"+d.getDate()+"/"+d.getFullYear();
+					return dFmt;
+				}.property("dateCreated")
+			});
+
+			for (var i = 8; i >= 1; i--) {
+				var tempObj = ImageRecord.create({id:i,caption:"Image "+i,
+					description:"This is a nature image "+i,
+					src:"http://lorempixel.com/400/200/nature/"+i,
+					dateCreated: new Date().setDate(Math.random()*30)
+				});
+				// tempObj.reopen({
+				// 	dateCreatedCalc:function(){
+				// 		var d = new Date(this.get("dateCreated"));
+				// 		var dFmt = (d.getMonth()+1)+"/"+d.getDate()+"/"+d.getFullYear();
+				// 		return dFmt;
+				// 	}.property("dateCreated")
+				// });
+				data.push(tempObj);
+			}
+			defer.resolve(data);
+		},0);
+
+		return defer.promise();
+	}
+});
+
+App.ImagesIndexController = Em.Controller.extend({
+	queryParams:["filter"],
+	filter:null,
+	filteredModel:Em.computed.oneWay("model"),
+	filterImages:function(){
+		if(!Em.isEmpty(this.get("model"))){
+			var filter = this.get("filter")?this.get("filter").trim():null;
+			if(Em.isEmpty(filter)){
+				this.set("filter",null);
+				this.set("filteredModel",this.get("model"));
+			}else{
+				var self = this;
+				var filteredModel = this.get("model").filter(function(elem) {
+					return elem.get("description").indexOf(self.get("filter")) !== -1;
+				});
+				this.set("filteredModel",filteredModel);
+			}
+			if(this.get("filteredModel").length==1){
+				this.transitionToRoute("images.image",this.get("filteredModel")[0].id);
+			}else{
+				this.transitionToRoute("images");
+			}
+		}
+	}.observes("filter","model")
+});
+
+App.ImagesImageRoute = Em.Route.extend({
+	model:function(params){
+		// console.log(">>>> model of ImagesImageRoute");
+		return this.modelFor("images").findBy("id",parseInt(params.id,10));
+	}
+});
+
+App.ImageDetailComponent = Em.Component.extend({
+	fadeComp:function(){
+		this.$().css("display","none");
+		var self = this;
+		Em.run.later(function(){
+			self.$().fadeIn("400");
+		});
+	}.on("didInsertElement").observes("model")
 });
